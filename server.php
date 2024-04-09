4a68ef6f9f4e614d89181c01c966bf857199fa7d
topanekka <126932631+topanekka@users.noreply.github.com>
2024-04-09 11:35:46 +0530

    Create server.php

---
+++ server.php
@@ -0,0 +1,17 @@
+#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-MEDIA-SEQUENCE:106634
+#EXT-X-TARGETDURATION:16
+#EXTINF:10.000, no desc
+mkv.php?ts=https://st77.ckthd.com/hls/token/dfgjhkazyfvbjs6ty9dtfbrvcfj78fy79k8/stream-106634-1712642341205.ts
+#EXTINF:10.280, no desc
+mkv.php?ts=https://st77.ckthd.com/hls/token/dfgjhkazyfvbjs6ty9dtfbrvcfj78fy79k8/stream-106635-1712642351163.ts
+#EXTINF:15.720, no desc
+mkv.php?ts=https://st77.ckthd.com/hls/token/dfgjhkazyfvbjs6ty9dtfbrvcfj78fy79k8/stream-106636-1712642361452.ts
+#EXTINF:10.000, no desc
+mkv.php?ts=https://st77.ckthd.com/hls/token/dfgjhkazyfvbjs6ty9dtfbrvcfj78fy79k8/stream-106637-1712642377159.ts
+#EXTINF:10.520, no desc
+mkv.php?ts=https://st77.ckthd.com/hls/token/dfgjhkazyfvbjs6ty9dtfbrvcfj78fy79k8/stream-106638-1712642387163.ts
+#EXTINF:10.000, no desc
+mkv.php?ts=https://st77.ckthd.com/hls/token/dfgjhkazyfvbjs6ty9dtfbrvcfj78fy79k8/stream-106639-1712642397758.ts
+
